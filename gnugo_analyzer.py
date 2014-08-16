b4ba9cfb1db8440f0b915637263c5f391036a8d8
Emilio <uc3709@yahoo.es>
2014-08-17 00:32:11 +0200

    gives ideas after some moves

--- gnugo_analyzer.py
+++ gnugo_analyzer.py
@@ -4,15 +4,16 @@ import os
 import sys
 import subprocess
 
-print len(sys.argv)
 if len(sys.argv) >= 2:
-    print "It seems we have a file to analyze"
     filename = sys.argv[1]
 else:
     print("usage: gnugo_analyzer my_file.sgf")
     exit()
 
 my_sgf_file = open(sys.argv[1], 'r')
+
+print my_sgf_file.name
+#exit()
 
 # How many moves are in the game?
 numlines = 0
@@ -23,24 +24,27 @@ for line in my_sgf_file:
     foundBlack += line.count(';B')
     foundWhite += line.count(';W')
      
-print foundWhite
-print foundBlack
-
 for move in range(max(foundBlack, foundWhite)):
-#    command = 'gnugo' + ' -l ' + str(my_sgf_file.name) + ' -L ' + str(move) + ' -T ' + ' -t ' + ' --level ' + '10'
-    command = ['gnugo', '-l ', str(my_sgf_file.name), '-L' , str(move), '-T',  '-t', '--level', '10']
-#    print command
-#    print os.devnull
-#    output = subprocess.Popen(command, stdout=subprocess.PIPE, shell = True)
-#    (out, err) = output.communicate()
-    p = subprocess.Popen(['gnugo', '-l ', str(my_sgf_file.name), '-L' , str(move), '-T',  '-t', '--level', '10'], stdout=subprocess.PIPE)
-    output, err = p.communicate()
-#    print output
-#    output = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()
-#    output = subprocess.Popen(command, stdout=open(os.devnull, 'w'))
-#    output = subprocess.call(command, shell=True)
-#    output = os.popen(command)
-#    output = subprocess.call(command)
-#    output = subprocess.Popen(command, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-#    output = subprocess.Popen((command), stdout = open(os.devnull, 'w'), shell = True)
-#    output = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
+    print
+    command = 'gnugo' + ' -l ' + str(my_sgf_file.name) + ' -L ' + str(move) + ' -T ' + ' -w ' + ' -t ' + ' --level ' + '10'
+    output = os.popen(command + " 2>&1")
+    tmpList = []
+    commentsList = []
+    count = 0
+    for line in output.readlines():
+        line = line.rstrip()
+        tmpList.append(line)
+        if "Move at" in line:
+            commentsList.append(line)
+    for info in tmpList:
+        if "Top" in info:
+            bestMove = tmpList[count + 1]
+            bestMove = bestMove[3:6]
+            print "Best move #",str(move+1), " is:", bestMove
+            for comment in commentsList:
+                if bestMove in comment:
+                    print comment
+        count+=1
+    
+    
+
