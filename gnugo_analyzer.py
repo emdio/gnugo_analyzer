fac0e3c0a548841be6653f5bca93eaa5f43bdcf9
Emilio <uc3709@yahoo.es>
2014-08-18 11:47:35 +0200

    Adds comments to the real move

--- gnugo_analyzer.py
+++ gnugo_analyzer.py
@@ -4,29 +4,83 @@ import os
 import sys
 import subprocess
 
+def convertSgf2Move(move):    
+    # Takes a sgf move like v.g."pd" and returns a human one "Q16"
+    
+    # Clean the move
+    move = move.replace('W[', '')
+    move = move.replace('B[', '')
+    move = move.replace(']', '')
+    
+    firstChar = move[0]
+    secondChar = move[1]
+    
+    # Convert fist char to the capital case
+    # If the char is "larger" than h we have to take into account
+    # the removed character in the "human" notation
+    if ord(firstChar) <= ord('h'):
+        firstChar = firstChar.capitalize()
+    else:
+        firstChar = chr(ord(firstChar)+1)
+        firstChar = firstChar.capitalize()
+    
+    # Convert the second char to a number from 1 to 19
+    secondChar = 19 - (ord(secondChar) - 97)
+    
+    return firstChar + str(secondChar)
+    
+    
+
 if len(sys.argv) >= 2:
     filename = sys.argv[1]
 else:
     print("usage: gnugo_analyzer my_file.sgf")
     exit()
 
-my_sgf_file = open(sys.argv[1], 'r')
+sgfFile = open(sys.argv[1], 'r')
+sgfContent = sgfFile.read().replace('\n', '')
+#for line in sgfFile.readlines():
+##        if ";B" in line:
+#        print line
+print sgfFile.name
 
-print my_sgf_file.name
-#exit()
+# Load file contento into a list
+sgfName = sgfFile.name
+sgfFile.close()
 
 # How many moves are in the game?
-numlines = 0
 foundWhite = 0
 foundBlack = 0
-for line in my_sgf_file:
-    numlines += 1
-    foundBlack += line.count(';B')
-    foundWhite += line.count(';W')
-     
-for move in range(max(foundBlack, foundWhite)):
+foundBlack += sgfContent.count(';B')
+foundWhite += sgfContent.count(';W')
+numMoves = max(foundWhite, foundBlack)
+    
+print numMoves
+
+# Cleaing the moves played in the game
+sgfContent = sgfContent.split(";")
+onlyMoves = []
+for field in sgfContent:
+#    print field
+    if 'W[' in field[0:5] or 'B[' in field[0:5]:
+        onlyMoves.append(field[0:5])
+print sgfContent
+print '************************'
+print onlyMoves
+print onlyMoves[0]
+print convertSgf2Move(onlyMoves[0])
+print onlyMoves[1]
+print convertSgf2Move(onlyMoves[1])
+print len(onlyMoves)
+
+#exit()
+
+# Analyzing the game
+for move in range(numMoves):
+#for move in range(2):
     print
-    command = 'gnugo' + ' -l ' + str(my_sgf_file.name) + ' -L ' + str(move) + ' -T ' + ' -w ' + ' -t ' + ' --level ' + '10'
+    command = 'gnugo' + ' -l ' + sgfName + ' -L ' + str(move+1) + ' -T ' + ' -w ' + ' -t ' + ' --level ' + '15'
+    print command
     output = os.popen(command + " 2>&1")
     tmpList = []
     commentsList = []
@@ -45,6 +99,12 @@ for move in range(max(foundBlack, foundWhite)):
                 if bestMove in comment:
                     print comment
         count+=1
+    yourMove = convertSgf2Move(onlyMoves[move])
+    print 'Your move was:', yourMove
+    for comment in commentsList:
+                if yourMove in comment:
+                    print comment
+    
     
     
 
